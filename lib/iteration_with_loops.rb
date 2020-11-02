3ac896a9142a10a9542d58d671a3b97bc74594e5
samumar43 <samumar43@gmail.com>
2020-11-02 03:48:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ outer_results = []
 row_index = 0 
 while row_index < src.count do
   element_index = 0 
+  min = src[row_index].min
   while element_index < src[row_index].count do
      end
     element_index += 1 
